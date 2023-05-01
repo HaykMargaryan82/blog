8399f3ff485701cba37bdbaf78eba90381623d20
HaykMargaryan82 <hayk.margaryan.1982@gmail.com>
2023-05-01 18:14:31 +0400

    Add admin template

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,6 @@
 <?php
+
+
 use App\Http\Controllers\Main\IndexController;
 use Illuminate\Support\Facades\Route;
 
@@ -13,10 +15,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::group(['namespace' => ''], function () {
+Route::group(['namespace' => 'App\\Http\\Controllers\\Main'], function () {
     Route::get('/', IndexController::class);
 });
 
+Route::group(['namespace' => 'App', 'prefix' => 'admin'], function () {
+   Route::group(['namespace' => 'Http'], function () {
+        Route::get('/', Controllers\Admin\Main\IndexController::class);
+    });
+});
 Auth::routes();
 
 //Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
