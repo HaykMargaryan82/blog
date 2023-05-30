9f051b5dea7cd046ac1ced6afde5f3c09d6ee315
HaykMargaryan82 <hayk.margaryan.1982@gmail.com>
2023-05-31 00:27:23 +0400

    Addadmin files

---
+++ resources/views/admin/user/edit.blade.php
@@ -0,0 +1,78 @@
+@extends('admin.layouts.main')
+@section('content')
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <div class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1 class="m-0">Dashboard</h1>
+                    </div><!-- /.col -->
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-right">
+                            <li class="breadcrumb-item"><a href="#">Home</a></li>
+                            <li class="breadcrumb-item active">Dashboard v1</li>
+                        </ol>
+                    </div><!-- /.col -->
+                </div><!-- /.row -->
+            </div><!-- /.container-fluid -->
+        </div>
+        <!-- /.content-header -->
+
+        <!-- Main content -->
+        <section class="content">
+            <div class="container-fluid">
+                <!-- Small boxes (Stat box) -->
+
+                <div class="row ">
+
+                    <div class="col-12"><h4>Edit Category</h4></div>
+                    <div class="col-12">
+                        <form action="{{route('admin.user.update',$user->id)}}"method="post" class="w-25">
+                            @csrf
+                            @method('Patch')
+                            <div class="form-group">
+                                <label >Name</label>
+                                <input type="text" class="form-control" name="name"  placeholder="User name"value="{{$user->name}}">
+                                @error('name')
+                                <div class="text-danger">{{$message}}</div>
+                                @enderror
+                            </div>
+                            <div class="form-group">
+                                <label >Email</label>
+                                <input type="text" class="form-control" value="{{$user->email}}" name="email"  placeholder="Email">
+                                @error('email')
+                                <div class="text-danger">{{$message}}</div>
+                                @enderror
+                            </div>
+                            <div class="form-group w-50">
+                                <label>Choose user</label>
+                                <select class="form-control" name="role_id">
+                                    @foreach($roles as $id => $role)
+                                        <option value="{{$id}}"
+                                            {{$id == $user->role ? 'selected': ''}}>
+                                            {{$role}}
+                                        </option>
+                                    @endforeach
+                                </select>
+                                @error('role_id')
+                                <div class="text-danger">{{$message}}</div>
+                                @enderror
+                            </div>
+                            <div class="form-group w-50">
+                               <input type="hidden"name="user_id"value="{{$user->id}}">
+                            </div>
+                            <input type="submit"class="btn btn-primary px-3"value="Refresh">
+                        </form>
+                    </div>
+
+                </div>
+                <!-- /.row -->
+                <!-- Main row -->
+
+                <!-- /.row (main row) -->
+            </div><!-- /.container-fluid -->
+        </section>
+        <!-- /.content -->
+    </div>
+@endsection
